40d17534978dff1a8cedd9f2255cd17c09d8c5a8
Matthew Alvarado-Ross <maalvaradoross@wpi.edu>
2020-06-02 17:08:05 -0400

    Updates

---
+++ precache-manifest.2a5b1e63940d9505662aaeb8afe79e61.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be6108f1fe2f3eaa142106a0aadacb3b",
+    "url": "/stem-cards-react/index.html"
+  },
+  {
+    "revision": "133219bf1cafccf00cde",
+    "url": "/stem-cards-react/static/css/main.0a5608a7.chunk.css"
+  },
+  {
+    "revision": "34683481b6c15caaef3e",
+    "url": "/stem-cards-react/static/js/2.4ce70efd.chunk.js"
+  },
+  {
+    "revision": "9535f847ae2a7a46497ee1b161fa54d8",
+    "url": "/stem-cards-react/static/js/2.4ce70efd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "133219bf1cafccf00cde",
+    "url": "/stem-cards-react/static/js/main.b4f09884.chunk.js"
+  },
+  {
+    "revision": "68bebb5953015b394329",
+    "url": "/stem-cards-react/static/js/runtime-main.6377b5fc.js"
+  },
+  {
+    "revision": "4d548a176f509174f9d1c3fb159a6bc9",
+    "url": "/stem-cards-react/static/media/stars4.4d548a17.gif"
+  }
+]);
